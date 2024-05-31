078df3b2042a2e8b79616633915934492ac8189b
YounessElQadery0 <158784197+YounessElQadery0@users.noreply.github.com>
2024-05-31 04:07:49 +0200

    Initial commit

---
+++ iTimer/iTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTimerApp.swift
+//  iTimer
+//
+//  Created by Youness El Qadery on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
